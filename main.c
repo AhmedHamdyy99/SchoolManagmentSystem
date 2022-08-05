5cee89805b551770f30cebf8fe7fa8c729accf47
AhmedHamdyy99 <85260561+AhmedHamdyy99@users.noreply.github.com>
2022-08-05 19:03:58 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "header.h"
+#include "header2.h"
+
+int main()
+{
+  int i=0;
+  Queue q;
+CreateQueue(&q);
+
+  while(i<100){
+   Main_Menu(&q);      //function to show Home Screen
+
+i++;
+  }
+
+
+
+
+    return 0;
+}
