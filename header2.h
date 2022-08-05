b8f5639c9ba4b32e846b977d8d79e57fbc605699
AhmedHamdyy99 <85260561+AhmedHamdyy99@users.noreply.github.com>
2022-08-05 19:04:58 +0200

    Add files via upload

---
+++ header2.h
@@ -0,0 +1,11 @@
+#ifndef HEADER2_H_INCLUDED
+#define HEADER2_H_INCLUDED
+#include "header.h"
+
+
+void wrapper(Queue *pq,int32_t choice);
+void CreateQueue(Queue *pq);
+void bubble_sorting(Queue *pq);
+
+
+#endif // HEADER2_H_INCLUDED
